e46a7fb054f11634e9470d5a31e3d79c4b69d06f
BM1012 <karatecabru@gmail.com>
2025-03-07 12:36:55 -0600

    Update 1_Asistencia.py

--- WEB/pages/1_Asistencia.py
+++ WEB/pages/1_Asistencia.py
@@ -308,7 +308,7 @@ if 'usuario' in st.session_state and 'area' in st.session_state:
                       y='R. EXCEDIDO',
                       title='Picos Retardos',
                       width=800,  # Ajusta el ancho de la gráfica
-                      height=405)  # Ajusta la altura de la gráfica
+                      height=407)  # Ajusta la altura de la gráfica
     fig2.update_traces(textposition='top center',
                        textfont=dict(size=16))
     # Agregar sombreado
